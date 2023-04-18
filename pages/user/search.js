4b9c01f6585e19338344539233dddb8d070d6940
legcy <legcy143@gmail.com>
2023-04-18 12:45:48 +0530

    min bug

--- pages/user/search.js
+++ pages/user/search.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import style from "./styles/search.module.scss"
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { useRouter } from 'next/router';
 
 const Search = () => {
+  let router = useRouter()
   return (
     <main>
-        <nav className=''>
-<h1>nav</h1>
+        <nav className={style.searchNav}>
+        <button className={style.backbtn} onClick={()=>{router.back(-1)}}><ArrowBackIosIcon/></button>
+        <div className={style.searchbox} > 
+                <input type="text" placeholder='search ..' />
+                <SearchIcon />
+            </div>
         </nav>
-        
     </main>
   )
 }
